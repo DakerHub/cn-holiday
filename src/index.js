04d0f1e73a2fb8c7c7c05008934a523897bf0082
Daker <523246656@qq.com>
2018-04-27 15:52:39 +0800

    [fix](index.js):修复index没有引用新增的方法

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const isHoliday = require('./isHoliday');
 const isRestday = require('./isRestday');
 const isWorkday = require('./isWorkday');
 const getHolidaysBetween = require('./getHolidaysBetween');
+const getWorkdaysBetween = require('./getWorkdaysBetween');
+const getWorkdaysCountBetween = require('./getWorkdaysCountBetween');
 
 module.exports = {
     isHoliday,
     isRestday,
     isWorkday,
-    getHolidaysBetween
+    getHolidaysBetween,
+    getWorkdaysBetween,
+    getWorkdaysCountBetween
 };
